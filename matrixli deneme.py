82b8f81c5d935e44e74fbfb887c346750ec8a193
afragultig <afragul.tigg@gmail.com>
2024-01-04 17:59:36 +0300

    allah kahretmesin al

--- matrixli deneme.py
+++ matrixli deneme.py
@@ -12,17 +12,17 @@ while second_player=='O' or not (second_player in string.ascii_letters) or first
 second_player=second_player.upper()
 first_player=first_player.upper()
 
-istek=int(input())
-tahtamatrix= [ [' ',' ',' ',' ',' ',' ',' ']]*7
+istek=int(input('tahta secin: '))
+gameboard= [ [' ',' ',' ',' ',' ',' ',' ']]*7
 
-def board3(game_board):
+def board3(gameboard):
     print('   A   B   C')
     print('  -----------')
-    print('1|' +' '+game_board[0][0]+' '+'|'+' '+game_board[0][1]+' '+'|'+' '+game_board[0][2]+' '+'|')
+    print('1|' +' '+gameboard[0][0]+' '+'|'+' '+gameboard[0][1]+' '+'|'+' '+gameboard[0][2]+' '+'|')
     print('  -----------')
-    print('2|' +' '+game_board[1][0]+' '+'|'+' '+game_board[1][1]+' '+'|'+' '+game_board[1][2]+' '+'|')
+    print('2|' +' '+gameboard[1][0]+' '+'|'+' '+gameboard[1][1]+' '+'|'+' '+gameboard[1][2]+' '+'|')
     print('  -----------')
-    print('3|' +' '+game_board[2][0]+' '+'|'+' '+game_board[2][1]+' '+'|'+' '+game_board[2][2]+' '+'|')
+    print('3|' +' '+gameboard[2][0]+' '+'|'+' '+gameboard[2][1]+' '+'|'+' '+gameboard[2][2]+' '+'|')
 
 def board5(game_board):
     print('   A   B   C   D   E')
@@ -57,84 +57,33 @@ def board7(game_board):
     print('  ---------------------------')
 
 if istek==3:
-    board3(tahtamatrix)
-    eski_konum1=tahtamatrix[0][1]
-    eski_konum2=tahtamatrix[2][1]
+    eski_konum1=[0,1]
+    eski_konum2=[2,1]
+    first_player = gameboard[eski_konum1[0]][eski_konum1[1]]
+    board3(gameboard)
 
 elif istek==5:
-    board5(tahtamatrix)
-    eski_konum1=tahtamatrix[0][2]
-    eski_konum2=tahtamatrix[4][1]
+    board5(gameboard)
+    eski_konum1=(0,2)
+    eski_konum2=(4,2)
 
 elif istek==7:
-    board7(tahtamatrix)
-    eski_konum1=tahtamatrix[0][3]
-    eski_konum2=tahtamatrix[6][1]
+    board7(gameboard)
+    eski_konum1=(0,3)
+    eski_konum2=(6,3)
 
-column_letter = {'A':0, 'B':1, 'C':2, 'D':3, 'E':4, 'F':5, 'G':6}
-row_num = {'1':0, '2':1, '3':2, '4':3, '5':4, '6':5, '7':6}
+def yenikonum(board,eski,player):
+    yon=input('yön seçin: ')
+    if yon=='S':
+        yeni_konum=(eski[0]+1,eski[1])
+        player = board[eski[0]][eski[1]]
+        board[eski[0]][eski[1]] = ' '
+        board[yeni_konum[0]][yeni_konum[1]] = player
+    return board[yeni_konum]
 
-def yon(konum_yonu,eski_konum):
-    global izin
-    global yeni_konum
-    if konum_yonu=='N':
-        if eski_konum[+0][+1]==' ':
-            izin=True
-            yeni_konum=eski_konum[+0][+1]
+first_player = gameboard[eski_konum1[0]][eski_konum1[1]]
 
-    elif konum_yonu=='S':
-        if eski_konum[+1][+0]==' ':
-            izin=True
-            yeni_konum=eski_konum[+1][+0]
-    
-    elif konum_yonu=='W':
-        if eski_konum[-1][+0]==' ':
-            izin=True
-            yeni_konum=eski_konum[-1][+0]
-    
-    elif konum_yonu=='E':
-        if eski_konum[+0][-1]==' ':
-            izin=True
-            yeni_konum=eski_konum[+0][-1]
-    
-    elif konum_yonu=='NW':
-        if eski_konum[-1][+1]==' ':
-            izin=True
-            yeni_konum=eski_konum[-1][+1]
-    
-    elif konum_yonu=='NE':
-        if eski_konum[+1][+1]==' ':
-            izin=True
-            yeni_konum=eski_konum[+1][+1]
-    
-    elif konum_yonu=='SE':
-        if eski_konum[+1][-1]==' ':
-            izin=True
-            yeni_konum=eski_konum[+1][-1]
-    
-    elif konum_yonu=='SW':
-        if eski_konum[-1][-1]==' ':
-            izin=True
-            yeni_konum=eski_konum[-1][-1]
+yenikonum((gameboard), eski_konum1 , first_player)
 
-    return yeni_konum, izin
-
-
-def konum(user_input, board_size):
-    pass
-    # column_letter = {'A':1, 'B':2, 'C':3, 'D':4, 'E':5, 'F':6, 'G':7}
-    # row_num = {'1':1, '2':2, '3':3, '4':4, '5':5, '6':6, '7':7}
-    # row=user_input[1]
-    # column =user_input[0].upper()
-    # return row_num[row]*board_size-(board_size-column_letter[column])
-
-for i in range(10):
-     #8 false tan asonra cilimali
-    istenen_konum1= input('gitmek istediginiz yönü yazin: ')
-    yon(istenen_konum1,eski_konum1)
-    eski_konum1=yeni_konum
-
-    istenen_konum2= input('gitmek istediginiz yönü yazin: ')
-    yon(istenen_konum2,eski_konum2)
-    eski_konum2=yeni_konum
-
+print(eski_konum1)
+print(board3(gameboard))
